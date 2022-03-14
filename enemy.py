31e1020ec33931535096bf84d4cd03535b4f5976
ByronGeorge <bgeorge978@gmail.com>
2022-03-13 22:25:34 -0700

    Enemy Class
    Created the enemy class template

---
+++ enemy.py
@@ -0,0 +1,9 @@
+class Enemy:
+
+    def __init__(self, health, mana, armor, magicResistance, magicDamage, physicalDamage):
+        self.health = health
+        self.mana = mana
+        self.armor = armor
+        self.magicResistance = magicResistance
+        self.magicDamage = magicDamage
+        self.physicalDamage = physicalDamage
