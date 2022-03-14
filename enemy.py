350166fec7a0ec6d1d59ddaadd309df7436d5377
chris-george67 <christopher.george676@gmail.com>
2022-03-14 01:41:59 -0400

    AddedNewEnemyParameters AddedNewEnemyParameters

--- enemy.py
+++ enemy.py
@@ -1,9 +1,15 @@
+from msilib import type_key
+
+
 class Enemy:
 
-    def __init__(self, health, mana, armor, magicResistance, magicDamage, physicalDamage):
+    def __init__(self, health, mana, armor, magicResistance, magicDamage, physicalDamage,enemyType,enemyName,dexterity):
         self.health = health
         self.mana = mana
         self.armor = armor
         self.magicResistance = magicResistance
         self.magicDamage = magicDamage
         self.physicalDamage = physicalDamage
+        self.enemyType = enemyType
+        self.enemyName = enemyName
+        self.dexterity = dexterity
