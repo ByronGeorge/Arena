59ce4e057f6a364725ec6eee6b67bc4dd1d71132
ByronGeorge <bgeorge978@gmail.com>
2022-03-13 22:46:53 -0700

    Updates
    SHow how to import class from another file and created the player.py class tempalte

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello world")
+import enemy
+
+e1 = enemy.Enemy(10, 10, 2, 1, 0, 4, 1, 1, 1)
+
+
+print(e1.health)
+print(e1.mana)
---
+++ player.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self, health, mana,):
+        self.health = health
+        self.mana = mana
+        #You fill the rest 
